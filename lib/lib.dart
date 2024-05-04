e4195fc66f6c97a18b203de696db542c582fd69d
Mohammed Zyoud <mohammed.zyoud1@gmail.com>
2024-05-04 19:11:18 +0300

    add library

---
+++ lib/lib.dart
@@ -0,0 +1,15 @@
+library lib;
+
+export "firebase_options.dart";
+export "forget_password.dart";
+export "custom_bulid.dart";
+export "setting_app.dart";
+export "home_screen.dart";
+export 'add_plant.dart';
+export "sqlite.dart";
+export "Signup.dart";
+export "report.dart";
+export "plant.dart";
+export "login.dart";
+export "main.dart";
+export "info.dart";
